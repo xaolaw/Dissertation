91978bc6b58f781c83b2220e167d2af5248e370b
Adam Ćwikła <adam6xaolaw@gmail.com>
2023-05-23 14:39:30 +0200

    Basic unit on board

---
+++ Assets/Units/Unit.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public GameObject unitPrefab;
+    public Vector3 scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = scale;
+        Vector3 unitPosition = new Vector3(0, 0.55f, 0);
+        Instantiate(unitPrefab, unitPosition, Quaternion.identity,transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
